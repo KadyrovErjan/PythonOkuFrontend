acb79c46163f4b85d917f78d51f8c7b913c9c12a
mmrek07-gif <mmrek07@gmail.com>
2026-08-19 19:18:48 +0600

    fixing the bugs

--- frontend/src/components/BrandLogo.jsx
+++
@@ -1,9 +0,0 @@
-export default function BrandLogo({ className = '' }) {
-  const classes = ['brand-mark', 'brand-logo', className].filter(Boolean).join(' ')
-
-  return (
-    <span className={classes} aria-hidden="true">
-      <img src="/logo.svg" alt="" />
-    </span>
-  )
-}
--- frontend/src/pages/student/Studentnotifications.jsx
+++
@@ -1,5 +0,0 @@
-import NotificationsPage from '../../components/NotificationsPage'
-
-export default function StudentNotifications() {
-  return <NotificationsPage role="student" />
-}
